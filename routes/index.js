e99b2306ef1fa78fc323f07e9e26597aacd40963
Alexandre Freitas <8220154@estg,ipp,pt>
2024-04-18 10:48:13 +0100

    V.3.0.0
    Continuacao da criacao do login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login/login', { title: 'Express' });
+});
+
+router.get('/recuperar', function(req, res, next) {
+  res.render('login/recuperar', { title: 'Express' });
+});
+
+router.get('/pass', function(req, res, next) {
+  res.render('login/pass', { title: 'Express' });
 });
 
 module.exports = router;
