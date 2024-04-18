96f91b94c977edde675507291be3c082fc1f62e9
Alexandre Freitas <8220154@estg,ipp,pt>
2024-04-18 11:43:05 +0100

    V.3.2.0

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,4 @@
+/*
 function visualizarpass() {
     var passwordField = document.getElementById("password");
     var toggleIcon = document.querySelector(".mudar");
@@ -10,3 +11,58 @@ function visualizarpass() {
         toggleIcon.textContent = "&#x1F441;"; // mudar para olhinho aberto
     }
 }
+*/
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const icon = document.querySelector('.mudar');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.textContent = '🙈';
+    } else {
+        passwordInput.type = 'password';
+        icon.textContent = '👁️';
+    }
+}
+
+
+/*// 
+
+Função para enviar o formulário
+        function submitForm() {
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const rememberMe = document.getElementById('rememberMe').checked;
+
+            // Simular autenticação (verificação de usuário/senha)
+            if (email === 'usuario@example.com' && password === 'senha') {
+                if (rememberMe) {
+                    // Salvar dados no armazenamento local
+                    localStorage.setItem('savedEmail', email);
+                    localStorage.setItem('rememberMe', 'true');
+                } else {
+                    // Limpar dados do armazenamento local
+                    localStorage.removeItem('savedEmail');
+                    localStorage.removeItem('rememberMe');
+                }
+                alert('Login bem-sucedido!');
+                // Redirecionar para a página de destino
+                // window.location.href = 'pagina-de-destino.html';
+            } else {
+                alert('Email ou senha inválidos.');
+            }
+        }
+
+*/
+
+window.onload = function() {
+    const rememberMe = localStorage.getItem('rememberMe');
+    if (rememberMe === 'true') {
+        const savedEmail = localStorage.getItem('savedEmail');
+        if (savedEmail) {
+            document.getElementById('email').value = savedEmail;
+            document.getElementById('rememberMe').checked = true;
+        }
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,12 @@ router.get('/pass', function(req, res, next) {
   res.render('login/pass', { title: 'Express' });
 });
 
+router.get('/pontos', function(req, res, next) {
+  res.render('pontos', { title: 'Express' });
+});
+
+router.get('/doar', function(req, res, next) {
+  res.render('doar', { title: 'Express' });
+});
+
 module.exports = router;
