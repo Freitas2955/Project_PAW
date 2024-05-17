da6caf38236d6b37cc5bbfa3c1fbdc16acbb8724
Duarte Sousa <126507544+DuarteMSousa@users.noreply.github.com>
2024-05-17 15:14:51 +0100

    V.4.2.0

---
+++ BackOffice/controllers/RestPointsController.js
@@ -0,0 +1,95 @@
+var mongoose = require("mongoose");
+var Points = require("../models/Points");
+
+var pointsController = {};
+
+mongoose
+  .connect(
+    "mongodb+srv://user:user@basepaw.e8ypv1l.mongodb.net/trabalhoPaw?retryWrites=true&w=majority&appName=BASEPAW"
+  )
+  .then(() => console.log("connection succesful"))
+  .catch((err) => console.error(err));
+/*
+pointsController.create = function (req, res) {
+  res.render("../views/pontos",{username: req.session.username,
+    userId: req.session.userId,});
+};*/
+
+pointsController.save = function (req, res) {
+  const point = new Points(req.body);
+  point
+    .save()
+    .then(() => {
+      console.log("Successfully created points.");
+      res.json({
+        point: req.body,
+        username: req.session.username,
+        userId: req.session.userId,
+      });
+      //res.render("../views/pontos");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ username: req.session.username, userId: req.session.userId });
+      //res.render("/pontos");
+    });
+};
+
+pointsController.edit = function (req, res) {
+  Points.findOne()
+    .then((point) => {
+      res.json({
+        point: point,
+        username: req.session.username,
+        userId: req.session.userId,
+      });
+      //res.render("../views/pontos");
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+};
+
+pointsController.update = function (req, res) {
+  Points.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        camisola: req.body.camisola,
+        acessorios: req.body.acessorios,
+        casaco: req.body.casaco,
+        calcas: req.body.calcas,
+        sapatos: req.body.sapatos,
+        roupainterior: req.body.roupainterior,
+        dinheiro: req.body.dinheiro,
+      },
+    },
+    { new: true }
+  )
+    .then((point) => {
+      res.redirect("/RestPoints");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/RestPoints");
+    });
+};
+
+pointsController.simulate = function (req, res) {
+  Points.findOne()
+    .then((point) => {
+      let id = req.params.id;
+      res.json({
+        point: point,
+        id: id,
+        username: req.session.username,
+        userId: req.session.userId,
+      })
+      //res.render("../views/donations/doar");
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+};
+
+module.exports = pointsController;
---
+++ BackOffice/controllers/RestRequestController.js
@@ -0,0 +1,260 @@
+var mongoose = require("mongoose");
+var Request = require("../models/Request");
+var Donator = require("../models/Donator");
+const bcrypt = require("bcryptjs");
+var path = require("path");
+var fs = require("fs");
+var requestController = {};
+
+mongoose
+  .connect(
+    "mongodb+srv://user:user@basepaw.e8ypv1l.mongodb.net/trabalhoPaw?retryWrites=true&w=majority&appName=BASEPAW"
+  )
+  .then(() => console.log("connection succesful"))
+  .catch((err) => console.error(err));
+
+requestController.management1 = function (req, res) {
+  let num;
+
+  (async () => {
+    try {
+      num = await Request.countDocuments({ done: true });
+      console.log("Número total de documentos:", num);
+      Request.find({ done: true })
+        .then((request) => {
+          console.log(req.session.userId);
+          res.json({
+            requests: request,
+            number: num,
+            username: req.session.username,
+            userId: req.session.userId,
+          });
+          //res.render("../views/requests/gestaoPedidosTerminados");
+        })
+        .catch((err) => {
+          console.log("Error:", err);
+        });
+    } catch (error) {
+      console.error("Ocorreu um erro ao contar os documentos:", error);
+    }
+  })();
+};
+
+requestController.management2 = function (req, res) {
+  let num;
+
+  (async () => {
+    try {
+      num = await Request.countDocuments({ done: false });
+      console.log("Número total de documentos:", num);
+      Request.find({ done: false })
+        .then((request) => {
+          console.log(req.session.userId);
+          res.json({
+            requests: request,
+            number: num,
+            username: req.session.username,
+            userId: req.session.userId,
+          });
+          //res.render("../views/requests/gestaoPedidosNaoTerminados");
+        })
+        .catch((err) => {
+          console.log("Error:", err);
+        });
+    } catch (error) {
+      console.error("Ocorreu um erro ao contar os documentos:", error);
+    }
+  })();
+};
+
+requestController.management1Find = function (req, res) {
+  let num;
+
+  (async () => {
+    try {
+      num = await Request.countDocuments({ done: true, city: req.query.city });
+      console.log("Número total de documentos:", num);
+      Request.find({ done: true, city: req.query.city })
+        .then((request) => {
+          console.log(req.session.userId);
+          res.json({
+            requests: request,
+            number: num,
+            username: req.session.username,
+            userId: req.session.userId,
+          });
+          //res.render("../views/requests/gestaoPedidosTerminados");
+        })
+        .catch((err) => {
+          console.log("Error:", err);
+        });
+    } catch (error) {
+      console.error("Ocorreu um erro ao contar os documentos:", error);
+    }
+  })();
+};
+
+requestController.management2Find = function (req, res) {
+  let num;
+
+  (async () => {
+    try {
+      num = await Request.countDocuments({ done: false, city: req.query.city });
+      console.log("Número total de documentos:", num);
+      Request.find({ done: false, city: req.query.city })
+        .then((request) => {
+          console.log(req.session.userId);
+          res.json({
+            requests: request,
+            number: num,
+            username: req.session.username,
+            userId: req.session.userId,
+          });
+          //res.render("../views/requests/gestaoPedidosNaoTerminados");
+        })
+        .catch((err) => {
+          console.log("Error:", err);
+        });
+    } catch (error) {
+      console.error("Ocorreu um erro ao contar os documentos:", error);
+    }
+  })();
+};
+
+/*
+requestController.list = function (req, res) {
+  Request.find()
+    .then((request) => {
+      res.render("../views/requests/showAll", { requests: request });
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+};
+*/
+
+requestController.show = function (req, res) {
+  Request.findOne({ _id: req.params.id })
+    .then((request) => {
+      res.json({
+        request: request,
+        username: req.session.username,
+        userId: req.session.userId,
+      });
+      //res.render("../views/requests/verpedido");
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+};
+
+/*
+requestController.create = function (req, res) {
+  res.render("../views/requests/create", {
+    username: req.session.username,
+    userId: req.session.userId,
+  });
+};*/
+
+requestController.save = function (req, res) {
+  Request.findOne({ donationId: req.params.id })
+    .then((request) => {
+      if (!request) {
+        Donator.findById({ _id: req.body.donatorId }).then((donator) => {
+          const data = {
+            donatorName: donator.name,
+            donationId: req.params.id,
+            address: donator.address,
+            postCode: donator.postCode,
+            city: donator.city,
+            done: false,
+          };
+          const request = new Request(data);
+          request.save().then((savedRequest) => {
+            console.log("Successfully created an Request.");
+            res.redirect("/RestRequests/naoTerminados");
+          });
+        });
+      } else {
+        console.log("Ja existe!");
+        res.redirect("/RestRequests/naoTerminados");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/RestRequests/");
+    });
+};
+/*
+requestController.edit = function (req, res) {
+  Request.findOne({ _id: req.params.id })
+    .then((request) => {
+      res.render("../views/utilizadores/editarpedido", {
+        request: request,
+        username: req.session.username,
+        userId: req.session.userId,
+      });
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+};
+
+requestController.update = function (req, res) {
+  Request.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        address: req.body.address,
+        postCode: req.body.postCode,
+        city: req.body.city,
+      },
+    },
+    { new: true }
+  )
+    .then((savedRequest) => {
+      console.log("Successfully created an Request.");
+
+      res.redirect("/users/gerirPedidos");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/users/gerirPedidos");
+    });
+};
+*/
+
+requestController.delete = function (req, res) {
+  Request.deleteOne({ _id: req.params.id })
+    .then(() => {
+      console.log("Request detected!");
+      res.redirect("/RestRequests/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+requestController.approve = function (req, res) {
+  Request.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        done: true,
+      },
+    },
+    { new: true }
+  )
+    .then((request) => {
+      res.redirect("/RestDonations/show/" + request.donationId);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/RestRequests/");
+    });
+};
+
+module.exports = requestController;
