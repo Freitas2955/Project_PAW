26459b1599ba6c812f92be8485c93b4081a9ce02
Alexandre Freitas <8220154@estg,ipp,pt>
2024-05-31 20:17:25 +0100

    V.9.1.0

--- FrontOffice/FrontOffice/src/app/navbar/doadores/editardoador/editardoador.component.ts
+++ FrontOffice/FrontOffice/src/app/navbar/doadores/editardoador/editardoador.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../navbar.component';
+import { RestService } from '../../../rest.service';
+import { Donator } from '../../../model/donator';
+import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editardoador',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FormsModule, CommonModule,ReactiveFormsModule],
   templateUrl: './editardoador.component.html',
   styleUrl: './editardoador.component.css'
 })
 export class EditardoadorComponent {
+  donator: Donator;
+  entidadeId :String|null=null;
+  confpassword?: String;
+  /////////////////////////////////
+  selectedFile: File;
 
+  imagePreview: string | ArrayBuffer | null = null;
+  ///////////////////////////////
+
+  constructor(private rest: RestService,private builder: FormBuilder,private route: ActivatedRoute) {
+    this.donator = new Donator();
+    const defaultContent = new Blob(['Conteúdo inicial'], { type: 'text/plain' });
+    this.selectedFile = new File([defaultContent], 'arquivoInicial.txt', { type: 'text/plain' });
+  }
+
+  ngOnInit(): void {
+    this.entidadeId = this.route.snapshot.paramMap.get('id');
+    this.getEntity();
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+      this.selectedFile=file;
+    }
+  }
+
+  getEntity(): void {
+    this.rest.getEntity(this.entidadeId).subscribe(
+      (response: any) => {
+        console.log('Resposta recebida:', response);
+        this.donator = response.entity;
+      },
+      (error) => {
+        console.error('Erro ao procurar entidade', error);
+      }
+    );
+  }
+
+
+  submitForm(): void {
+      console.log(this.donator);
+      this.rest.updateEntity(this.donator, this.selectedFile).subscribe(
+        (response) => {
+          console.log('Instituição registada com sucesso:', response);
+        },
+        (error) => {
+          console.error('Erro ao registar instituição:', error);
+        }
+      );
+  }
 }
