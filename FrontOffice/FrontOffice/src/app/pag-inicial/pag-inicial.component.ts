7adaa0ec462752731829e7c53e1e942f25a3b6b6
Duarte Sousa <126507544+DuarteMSousa@users.noreply.github.com>
2024-06-12 12:25:15 +0100

    V.14.0.0

--- FrontOffice/FrontOffice/src/app/pag-inicial/pag-inicial.component.ts
+++ FrontOffice/FrontOffice/src/app/pag-inicial/pag-inicial.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
+import { Entity } from '../model/entity';
+import { EntitiesService } from '../services/entities.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RestService } from '../services/rest.service';
+import { PartnersService } from '../services/partners.service';
+import { Partner } from '../model/partner';
 @Component({
   selector: 'app-pag-inicial',
   standalone: true,
@@ -11,7 +17,18 @@ import { CommonModule } from '@angular/common';
 })
 export class PagInicialComponent {
   show: boolean = false;
-  constructor(private router: Router) {}
+  searchName: string = '';
+  searchCity: string = '';
+  searchPhone: string = '';
+  entities: Entity[]=[new Entity()];
+  partners: Partner[] = [new Partner()];
+  filteredPartners: Partner[] = [new Partner()];
+  filteredEntities: any[] = [];
+  constructor(
+    public rest: EntitiesService,
+    private route: ActivatedRoute,
+    private router: Router,private sanitizer: DomSanitizer,public rest2: RestService,public rest3:PartnersService
+  ) {}
   loadLogin() {
     this.router.navigate(['/login']);
   }
@@ -35,4 +52,52 @@ export class PagInicialComponent {
   registerPartner() {
     this.router.navigate(['/registarparceiro']);
   }
+
+  ngOnInit(): void {
+    this.getEntities();
+    this.getPartners();
+  }
+
+  getEntities(): void {
+    console.log('getEntities chamado');
+    this.rest.getEntities().subscribe((response: any) => {  
+      console.log('Resposta recebida:', response);
+      this.entities = response.entities;  
+      this.entities.forEach(entity=>{
+        let imageObservable;
+        imageObservable = this.rest2.getEntityImage(entity._id);
+        imageObservable.subscribe((imageBlob) => {
+          const objectURL = URL.createObjectURL(imageBlob);
+          entity.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+        });
+        this.filteredEntities=this.entities;
+      })
+    }, error => {
+      console.error('Erro ao procurar entidade', error);
+    });
+
+  }
+
+  getPartners(): void {
+    console.log('getPartners chamado');
+    this.rest3.getPartners().subscribe(
+      (response: any) => {
+        console.log('Resposta recebida:', response);
+        this.partners = response.partners;
+        this.partners.forEach((partner) => {
+          let imageObservable;
+          imageObservable = this.rest2.getPartnerImage(partner._id);
+          imageObservable.subscribe((imageBlob) => {
+            const objectURL = URL.createObjectURL(imageBlob);
+            partner.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+          });
+        });
+        this.filteredPartners = this.partners;
+      },
+      (error) => {
+        console.error('Erro ao procurar parceiro', error);
+      }
+    );
+  }
+
 }
